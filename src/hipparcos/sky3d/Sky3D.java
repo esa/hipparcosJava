ab798d969131319d36f7ebdcb2afb48d8d953b58
William O'Mullane <womullan@sciops.esa.int>
2016-04-07 13:18:37 +0200

    headers

--- src/hipparcos/sky3d/Sky3D.java
+++ src/hipparcos/sky3d/Sky3D.java
@@ -1,8 +1,22 @@
-package hipparcos.sky3d;
+
 /*
-	Sky3D some stuff in 3d
-	Ripped off from one of suns demos mainly (Bilboard).
- */
+* Copyright (C) 1997-2016 European Space Agency
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package hipparcos.sky3d;
 
 import java.awt.BorderLayout;
 import java.awt.event.*;
